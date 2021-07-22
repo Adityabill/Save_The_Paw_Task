2b349c9c882a3f09006dd19f27682833728088b1
Adityabill <adityabikrambill@gmail.com>
2021-07-22 21:28:00 +0530

    Added files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Our Blogs</title>
+   
+        <?php
+        include('templates/header.php')
+        ?>
+
+    </body>
+</html>
