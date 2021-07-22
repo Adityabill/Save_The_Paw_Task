c2771fab1dbb5cdedccc7b1b7ed8904d12a169b7
Adityabill <adityabikrambill@gmail.com>
2021-07-22 22:20:23 +0530

    Added files

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <?php
 
+
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <title>Our Blogs</title>
-   
+        <link href="Static/Main.css" rel="stylesheet">
+   <h1 class="head-text-align">Save the Paws</h1>
         <?php
         include('templates/header.php')
         ?>
+
+
+<section>
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+</section>
 
     </body>
 </html>
