ef4a1c7e88ec1842ef824db9b4b13a472e0c9850
Adityabill <adityabikrambill@gmail.com>
2021-07-23 12:44:34 +0530

    Updated

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
+$conn = mysqli_connect('localhost:8086', 'Bill', 'bill033', 'SaveThePaws');
+
+if(!$conn){
+    echo "Connection error: ". mysqli_connect_error();
+}
+
+$sql = 'select * from blogs';
+
+//storing the query results in '$result' variable 
+$result = mysqli_query($conn, $sql);
+
+//fetching the result as an array
+$blogs = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+//freeing the result from memory
+mysqli_free_result($result);
+
+//closing the database connection
+mysqli_close($conn);
 
 ?>
 
@@ -16,17 +36,19 @@
 
 
 <section>
-
-<div class="blog-div">
+<?php foreach($blogs as $blog){ ?>
+<div class="blog-div img-boundary">
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="Images/Blog_img.jpg" class="card-img-top img-boundary" alt="Blog Image">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title"><?php echo htmlspecialchars($blog['Blog_Title']); ?></h5>
+    <p><small>By - <?php echo htmlspecialchars($blog['Blog_Author']) ?> </small> </p>
+    <p><?php echo htmlspecialchars($blog['Blog_Content']) ?> </p>
+    <p><small>Posted on: <?php echo htmlspecialchars($blog['Posted_On']); ?></small></p>
   </div>
 </div>
 </div>
+<?php } ?>
 
 
 </section>
--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <title></title>
+        <link href="Static/Main.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
@@ -11,23 +12,29 @@
         .header_txt{
         color: white;
         }
+
+        .nav-link{
+          color: white;
+        }
     </style>
 
     <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse" id="navbarToggleExternalContent">
+  <div class="bg-dark p-4">
+    <h5 class="text-white h4">Our Blogs</h5>
+    <ul>
+    <li><a href="" class="nav-link">Home</a></li>
+    <li><a href="" class="nav-link">Our Blogs</a></li>
+    <li><a href="" class="nav-link">Contact Us</a></li>
+      </ul>
+  </div>
+</div>
+<nav class="navbar navbar-dark bg-dark">
   <div class="container-fluid">
-    <h1 class="navbar-brand">Our Blogs</h1>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Our Blogs</a>
-        <a class="nav-link" href="#">About Us</a>
-      </div>
-    </div>
   </div>
 </nav>
     
